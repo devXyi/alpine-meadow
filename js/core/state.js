69652c5b6dd0dcc1a229ef4caac28441f55d406d
Devmani Tripathi <tripathidevmani023@gmail.com>
2026-08-09 23:12:28 +0530

    feat: add time pause state

--- js/core/state.js
+++ js/core/state.js
@@ -1,47 +1,13 @@
-// A single shared, mutable object for runtime state.
 export const state = {
-  scene: null,
-  camera: null,
-  renderer: null,
-  canvas: null,
-  character: null,
-
-  dayTime: 0.74,
-  nightFactor: 0,
-  weatherMode: 'clear',
-  running: false,
-  windStrength: 1,
-
-  cameraYaw: 0,
-  cameraPitch: -0.28,
-
-  grassShaderRef: null,
-  waterShaderRef: null,
-  flowerShaderRefs: [],
-
-  trees: [],
-  clouds: [],
-  flyers: [],
-  fish: [],
-  rabbits: [],
-  windowGlowMats: [],
-  lanternGlows: [],
-  smokePuffs: [],
-
-  fireflyHomes: [],
-  fireflyGeo: null,
-  fireflyMat: null,
-
-  weatherGeo: null,
-  weatherMat: null,
-  weatherPoints: null,
-  weatherSeeds: null,
-
-  starMat: null,
-  skyMat: null,
-  sunLight: null,
-  moonLight: null,
-  hemiLight: null,
-  sunSprite: null,
-  moonSprite: null
+  scene: null, camera: null, renderer: null, canvas: null, character: null,
+  dayTime: 0.74, nightFactor: 0, weatherMode: 'clear',
+  running: false, timePaused: false, windStrength: 1,
+  cameraYaw: 0, cameraPitch: -0.28,
+  grassShaderRef: null, waterShaderRef: null, flowerShaderRefs: [],
+  trees: [], clouds: [], flyers: [], fish: [], rabbits: [],
+  windowGlowMats: [], lanternGlows: [], smokePuffs: [],
+  fireflyHomes: [], fireflyGeo: null, fireflyMat: null,
+  weatherGeo: null, weatherMat: null, weatherPoints: null, weatherSeeds: null,
+  starMat: null, skyMat: null, sunLight: null, moonLight: null,
+  hemiLight: null, sunSprite: null, moonSprite: null
 };
