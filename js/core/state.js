2fccc74858753a82c7ba54b7b12e08b648bcac1e
Devmani Tripathi <tripathidevmani023@gmail.com>
2026-08-09 23:29:33 +0530

    Track aurora and world detail state

--- js/core/state.js
+++ js/core/state.js
@@ -1,7 +1,7 @@
 export const state = {
   scene: null, camera: null, renderer: null, canvas: null, character: null,
   dayTime: 0.74, nightFactor: 0, weatherMode: 'clear',
-  running: false, timePaused: false, windStrength: 1,
+  running: false, timePaused: false, windStrength: 1, auroraEnabled: true,
   cameraYaw: 0, cameraPitch: -0.28,
   grassShaderRef: null, waterShaderRef: null, flowerShaderRefs: [],
   trees: [], clouds: [], flyers: [], fish: [], rabbits: [],
@@ -9,5 +9,6 @@ export const state = {
   fireflyHomes: [], fireflyGeo: null, fireflyMat: null,
   weatherGeo: null, weatherMat: null, weatherPoints: null, weatherSeeds: null,
   starMat: null, skyMat: null, sunLight: null, moonLight: null,
-  hemiLight: null, sunSprite: null, moonSprite: null
+  hemiLight: null, sunSprite: null, moonSprite: null,
+  aurora: null, auroraMaterial: null, campfire: null
 };
